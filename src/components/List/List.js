b39658c73b78c46f2e7f96f90e5b30f4756495dd
Franek Zawada <franek.zawada@gmail.com>
2020-04-25 17:23:12 +0200

    Add Container

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
 import styles from './List.scss';
 import {settings} from '../../data/dataStore';
-import Hero from '../Hero/Hero.js';
-import Column from '../Column/ColumnContainer.js';
-import Creator from '../Creator/Creator.js';
+import Hero from '../Hero/Hero';
+import Column from '../Column/ColumnContainer';
+import Creator from '../Creator/Creator';
+import Container from '../Container/Container';
 
 class List extends React.Component {
 
@@ -24,20 +25,22 @@ class List extends React.Component {
   render() {
     const {title, image, description, columns, addColumn} = this.props;
     return (
-      <section className={styles.component}>
-        <Hero title={title} image={image}/>
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
-        <div className={styles.columns}>
-          {columns.map(columnData => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn}/>
-        </div>
-      </section>
+      <Container>
+        <section className={styles.component}>
+          <Hero title={title} image={image}/>
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn}/>
+          </div>
+        </section>
+      </Container>
     );
   }
 }
